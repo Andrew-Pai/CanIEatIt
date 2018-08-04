bf88ccb6bf4ceb49a9521ee35d5c058817fff8f5
Andrew Pai <akpai@edu.uwaterloo.ca>
2018-08-04 01:27:13 -0400

    Fixed UI elements

---
+++ app/src/main/java/io/github/andrew_pai/canieatit/FoodDatabase.java
@@ -0,0 +1,35 @@
+package io.github.andrew_pai.canieatit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class FoodDatabase extends AsyncTask <Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... params) {
+        String response="";
+        try {
+            URL API = new URL("https");
+            HttpsURLConnection myConnection = (HttpsURLConnection) API.openConnection();
+
+            myConnection.setRequestProperty("e3c68478","bbf29319775e2048d28630abf5e4f59e");
+
+            if(myConnection.getResponseCode() == 200) {
+                InputStream responseBody = myConnection.getInputStream();
+            }
+
+            return response;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
--- app/src/main/java/io/github/andrew_pai/canieatit/MainActivity.java
+++ app/src/main/java/io/github/andrew_pai/canieatit/MainActivity.java
@@ -2,17 +2,34 @@ package io.github.andrew_pai.canieatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView output;
+    EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        RelativeLayout relativeLayout = findViewById(R.id.mainLayout);
+        output = findViewById(R.id.output);
+        input = findViewById(R.id.FoodInput);
 
-
+        Button search = findViewById(R.id.search_button);
+        search.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        String food = input.getText().toString();
+        output.setText(food);
+    }
+
 }
